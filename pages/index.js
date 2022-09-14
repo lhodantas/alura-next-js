dc7b0b4b0c6b7de3c5ef341520e8a24f7642463f
lhodantas <leonardohod@hotmail.com>
2022-09-14 13:01:20 -0400

    primeiro commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen;
+// import Link from "../src/components/Link";
+// import Footer from "../src/components/patterns/Footer";
+
+// function Title({ children, as }) {
+//   const Tag = as;
+//   return (
+//     <>
+//       <Tag> {children} </Tag>
+
+//       <style jsx>{`
+//         ${as} {
+//           color: red;
+//         }
+//       `}</style>
+//     </>
+//   );
+// }
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as="h1"> Alura Cases</Title>
+//       <Link href="/faq">Ir para a FAQ</Link>
+//       <Footer />
+//     </div>
+//   );
+// }
